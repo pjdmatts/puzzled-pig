58ccd43a89032dea2fe60227f0ce43aefbc95333
Peter Matthews <pjdmatthews7@gmail.com>
2015-08-23 11:29:07 -0400

    Add Home and About

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
+
   resources :points
   resources :trails
   # The priority is based upon order of creation: first created -> highest priority.
